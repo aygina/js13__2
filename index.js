b2df30ea5e27f01a9a5fa16c402d367bb4d17241
aygina <aygina.v@gmail.com>
2022-10-16 17:00:55 +0300

    js

---
+++ index.js
@@ -0,0 +1,23 @@
+function sum() {
+    let a = prompt('Введите первое число');
+    let b = prompt('Введите второе число');
+    alert(Number(a) + Number(b));
+}
+
+function subtract() {
+    let a = prompt('Введите первое число');
+    let b = prompt('Введите второе число');
+    alert(a - b);
+}
+
+function multiply() {
+    let a = prompt('Введите первое число');
+    let b = prompt('Введите второе число');
+    alert(a * b);
+}
+
+function divide() {
+    let a = prompt('Введите первое число');
+    let b = prompt('Введите второе число');
+    alert(a / b);
+}
